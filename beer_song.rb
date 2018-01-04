6dc562e7b11f25753f905a9a294580db47876b3d
Mark R <mirubenstein@gmail.com>
2018-01-03 21:32:35 -0800

    Add solution with verse map.

--- beer_song.rb
+++ beer_song.rb
@@ -1,35 +1,47 @@
 class BeerSong
+
   def verse(number)
-    case number
-      when 0
-        <<-TEXT
+    verse_map[number]
+  end
+
+  def verse_map
+    @verse_map ||= begin
+      Hash.new do |hash, key|
+        hash[key] = <<-TEXT
+#{key} bottles of beer on the wall, #{key} bottles of beer.
+Take one down and pass it around, #{key - 1} bottles of beer on the wall.
+        TEXT
+      end.tap do |obj|
+        obj.merge!(
+          {
+            0 =>
+              <<-TEXT,
 No more bottles of beer on the wall, no more bottles of beer.
 Go to the store and buy some more, 99 bottles of beer on the wall.
-        TEXT
-      when 1
-        <<-TEXT
+            TEXT
+            1 =>
+              <<-TEXT,
 1 bottle of beer on the wall, 1 bottle of beer.
 Take it down and pass it around, no more bottles of beer on the wall.
-        TEXT
-      when 2
-        <<-TEXT
+            TEXT
+            2 =>
+              <<-TEXT,
 2 bottles of beer on the wall, 2 bottles of beer.
 Take one down and pass it around, 1 bottle of beer on the wall.
-        TEXT
-      else
-      <<-TEXT
-#{number} bottles of beer on the wall, #{number} bottles of beer.
-Take one down and pass it around, #{number - 1} bottles of beer on the wall.
-      TEXT
+            TEXT
+          }
+        )
+      end
     end
   end
 
   def verses(verse_start, verse_end)
     verse_start.downto(verse_end).each_with_object([]) do |number, verses|
-      verses << verse(number)
+      verses << verse_map[number]
     end.join("\n")
   end
 end
+
 
 module BookKeeping
   VERSION = 3
